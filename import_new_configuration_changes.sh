822c7ada17e7506d16d4613ff141caf7df036e29
Sacha-Lewis <sachalewis@undologic.com>
2024-06-15 10:08:36 -0400

    importing new configuration.nix changes

--- import_new_configuration_changes.sh
+++ import_new_configuration_changes.sh
@@ -2,11 +2,11 @@
 # copy the new changes
 sudo cp /etc/nixos/configuration.nix ~/nixos/configuration.nix
 
+# accept all files
+git add *
+
 git commit -m "importing new configuration.nix changes"
 
 git push origin master
-
-
-
 
 #### new
