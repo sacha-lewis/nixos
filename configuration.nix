840cd2bdbd2e9bb186fa2f520f1113cc2600d554
Sacha-Lewis <sachalewis@undologic.com>
2024-06-17 02:47:46 -0400

    importing new configuration.nix changes

--- configuration.nix
+++ configuration.nix
@@ -129,11 +129,12 @@ environment.systemPackages = with pkgs; [
 	xdotool
 	tailscale
 	xmlstarlet
+
+	mysql80
 #	php
 # vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
 # wget
 ];
-
 
 
 
