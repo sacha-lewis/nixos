16ecea64d48deb9bf457abc3d39fd1dcd45f59b6
Sacha-Lewis Dmytruk <lewis@undologic.com>
2024-06-17 03:52:59 -0400

    Argument for git commit

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Install NixOS
 
+Add user that matches the configuration.nix file
+
+useradd -m sachalewis
+
 Sync home dir
 ```angular2html
 cd ~/home
--- import_new_configuration_changes.sh
+++ import_new_configuration_changes.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
+
+# Check if an argument is provided
+if [ -z "$1" ]; then
+  echo "Error: No commit message provided."
+  echo "Usage: $0 'commit message'"
+  exit 1
+fi
+
 # copy the new changes
 sudo cp /etc/nixos/configuration.nix ~/nixos/configuration.nix
 
 # accept all files
 git add *
 
-git commit -m "importing new configuration.nix changes"
+git commit -m "$1"
 
 git push origin master
 
