719080d7407329c346c8050be9112eab63ad17d2
Sacha-Lewis Dmytruk <lewis@undologic.com>
2024-06-17 03:25:37 -0400

    warning

--- overwrite_switch_configuration.sh
+++ overwrite_switch_configuration.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 sudo cp ~/nixos/configuration.nix /etc/nixos/configuration.nix
-read -p "CAREFUL: Did you set the password for the user ? Press CTRL+C and do this now otherwise press enter to nixos-rebuild switch"
+read -p "- - - ADD USER !!! IMPORTANT !!! - - - CAREFUL: Did you set the password for the user ? Press CTRL+C and do this now otherwise press enter to nixos-rebuild switch"
 sudo nixos-rebuild switch
